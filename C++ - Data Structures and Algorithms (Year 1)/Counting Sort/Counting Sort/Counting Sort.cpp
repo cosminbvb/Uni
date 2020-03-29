daa95706e9cf57d6bb32d96f7375dee132e50bf0
Cosmin Petrescu <cosminfcpeue@gmail.com>
2020-03-29 21:20:30 +0300

    Added linear sorting algorithms

---
+++ C++ - Data Structures and Algorithms (Year 1)/Counting Sort/Counting Sort/Counting Sort.cpp
@@ -0,0 +1,52 @@
+﻿// Counting Sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+/*
+An important property of counting sort is that it is stable: numbers with the same value appear in the output array
+in the same order as they do in the input array. That is, it breaks ties between two numbers by the rule that whichever
+number appears ﬁrst in the input array appears ﬁrst in the output array. Normally, the property of stability is important
+only when satellite data are carried around with the element being sorted. Counting sort’s stability is important for another
+reason: counting sort is often used as a subroutine in radixort.
+*/
+void CountingSort(int* a, int size) {
+	int i;
+	int min = a[0], max = a[0];
+	for (i = 1; i < size; i++) {
+		if (a[i] < min)min = a[i];
+		if (a[i] > max)max = a[i];
+	}
+	int k = max - min;
+	int* b = new int[size]; //auxiliary array
+	int* c = new int[k+1]; //frequency array
+	for (i = 0; i <= k; i++) 
+		c[i] = 0;
+	for (i = 0; i < size; i++)
+		c[a[i]-min]++;
+	for (i = 1; i <= k; i++)
+		c[i] = c[i] + c[i - 1];
+	for (i = size - 1; i >= 0; i--) {
+		b[c[a[i]-min]-1] = a[i];
+		c[a[i]-min]--;
+	}
+	for (i = 0; i < size; i++) //overwrite
+		a[i] = b[i];
+	delete[]b;
+	delete[]c;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	int* a = new int[n];
+	for (int i = 0; i < n; i++) cin >> a[i];
+	CountingSort(a, n);
+	for (int i = 0; i < n; i++) cout << a[i] << " ";
+	delete[]a;
+
+	return 0;
+}
+
---
+++ C++ - Data Structures and Algorithms (Year 1)/Radix Sort/Radix Sort/Radix Sort.cpp
@@ -0,0 +1,59 @@
+﻿// Radix Sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+/*
+Given n d-digit numbers in which each digit can take on up to k possible values,
+radix-sort correctly sors these numbers in θ(d(n+k)) time is the stable sort it 
+uses takes θ(n+k) time.
+*/
+
+int getMax(int* a, int size) {
+	int max = a[0];
+	for (int i = 1; i < size; i++) {
+		if (a[i] > max) max = a[i];
+	}
+	return max;
+}
+
+void countSort(int* a, int size, int exp) {
+	int* b = new int[size]; //auxiliary array
+	int* c = new int[10]; //frequency array
+	int i;
+	for (i = 0; i <= 9; i++) 
+		c[i] = 0;
+	for (i = 0; i < size; i++)
+		c[(a[i] / exp) % 10]++;
+	for (i = 1; i <= 9; i++)
+		c[i] += c[i - 1];
+	for (i = size - 1; i >= 0; i--) {
+		b[c[(a[i] / exp) % 10] - 1] = a[i];
+		c[(a[i] / exp) % 10]--;
+	}
+	for (i = 0; i < size; i++)
+		a[i] = b[i];
+	delete[]b;
+	delete[]c;
+}
+
+void RadixSort(int* a, int size) {
+	int max = getMax(a, size);
+	//finds the biggest number to know the number of digits
+	for (int exp = 1; max / exp > 0; exp *= 10) {
+		countSort(a, size, exp);
+	}
+}
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	int* a = new int[n];
+	for (int i = 0; i < n; i++)
+		std::cin >> a[i];
+	RadixSort(a, n);
+	for (int i = 0; i < n; i++)
+		std::cout << a[i] << " ";
+	delete[]a;
+}
